385676d568f9e96ce0c9f10e9fc872414fd965c9
json burn <vbnmnbvbnm1234@gmail.com>
2021-07-03 22:48:42 +0800

    bump version to 2.1

--- Shared/AssemblyInfo.cs
+++ Shared/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("https://github.com/IllusionMods/PHMoreSlotID")]
 [assembly: AssemblyProduct("PHMoreSlotID for BepInEx")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -27,4 +27,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0")]
+[assembly: AssemblyVersion("2.1")]
